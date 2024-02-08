6faa88cb77a3b75e18a04e03d74ac32a126a0c43
ItaloFerreira21 <duduu21q@gmail.com>
2024-02-07 21:20:24 -0300

    feat: implementing error handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,15 +1,16 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controller.tag_creator_controller import TagCreatorController
 class TagCreatorView:
     '''
     responsabilidade para interagir com http
     '''
-    def validade_and_creat(self, http_request: HttpRequest ) -> HttpResponse :
-        #body = http_request.body
-        #product_code = body ["product_code"]
+    def validate_and_create(self, http_request: HttpRequest ) -> HttpResponse :
+        tag_creator_controller = TagCreatorController()
+        body = http_request.body
+        product_code = body ["product_code"]
         #Logicas de regra de negocio
-        print("estou na minha viwe")
-        print(http_request)
+        formatted_response = tag_creator_controller.create_tag(product_code)
         #retorno http
-        return HttpResponse(status_code=200, body={"helow": "word"})
+        return HttpResponse(status_code=200, body=formatted_response)
         
